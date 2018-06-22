2f0440bdb8ee784c26bb2d5c3f529cf75b56f4ea
Unknown <hudongrui_01@outlook.com>
2018-06-22 11:03:00 -0400

    Process the image within the bounding rectangles

--- performRecognition.py
+++ performRecognition.py
@@ -11,19 +11,21 @@ import time
 global blockWidth
 blockWidth = 89
 
-#Croping the center part of the number off
+# Croping the center part of the number off
 def centerCorrection(x, y, w, h):
 	multiplierx = abs(x - center_x) // center_x
 	multipliery = abs(y - center_y) // center_y
+	xfactor = 1.2
+	yfactor = 1.3
 
-	offset = w - blockWidth * (1 + multiplierx)
+	offset = int(w - xfactor * blockWidth * (1 + multiplierx))
 	if(x > center_x):
 		x = x + offset
 		w = blockWidth
 	else:
 		w = w - offset
 
-	offset = h - blockWidth * (1 + multipliery)
+	offset = int(h - yfactor * blockWidth * (1 + multipliery))
 	if(y > center_y):
 		y = y + offset
 		h = blockWidth
@@ -55,8 +57,6 @@ ret, im_th = cv2.threshold(im_gray, 100, 255, cv2.THRESH_BINARY_INV)
 # Find contours in the image
 _, ctrs, hier = cv2.findContours(im_th.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-
-
 # Debuggin
 colored = cv2.cvtColor(im_th, cv2.COLOR_GRAY2RGB)
 
@@ -72,15 +72,13 @@ colored = cv2.cvtColor(colored, cv2.COLOR_BGR2GRAY)
 
 _, ctrs, hier = cv2.findContours(colored.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-#for ctr in ctrs:
+# for ctr in ctrs:
 #	x,y,w,h = cv2.boundingRect(ctr)
 #	cv2.bitwise_not(colored)
 colored = cv2.bitwise_not(colored)
 
 kernel = np.ones((3,3),np.uint8)
 erode = cv2.erode(colored,kernel,iterations = 2)
-
-
 
 # # Test Erosion
 # erode_1 = erode
@@ -98,11 +96,6 @@ im_w, im_h, im_c = im.shape
 
 center_x = im_h/2
 center_y = im_w/2
-# cv2.circle(colored,(center_x, center_y), 2, (0,0,255), -1)
-
-cv2.imshow("Denoised", erode)
-cv2.waitKey()
-
 
 # Get rectangles contains each contour
 rects = [cv2.boundingRect(ctr) for ctr in ctrs]
@@ -127,22 +120,63 @@ for rect in rects:
     else:
         rects_split.append(rect)
 
+# Remove noises on cropped block images
+for rect in rects_split:
+    (x,y,w,h) = centerCorrection(rect[0],rect[1],rect[2], rect[3])
+    rect = []
+    rect.append(x)
+    rect.append(y)
+    rect.append(w)
+    rect.append(h)
+
+
+    pt1 = int(rect[1])
+    pt2 = int(rect[1] + rect[3])
+    pt3 = int(rect[0])
+    pt4 = int(rect[0] + rect[2])
+    region = erode[pt1:pt2, pt3:pt4]
+    mask = region.copy()
+
+    _, ctrs, hier = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    ### Below is NOT correct, change it so that it blurs the block region instead
+    for ctr in ctrs:
+    	bounding = cv2.boundingRect(ctr)
+
+    	if(w >= 5, h >= 20):
+    		cv2.fillPoly(region, ctr, [255, 255, 255])
+    		region = cv2.bitwise_and(mask, region)
+    		break	
+    #region = cv2.bitwise_not(region)
+    cv2.imshow("Noise Removed", region)
+    cv2.waitKey()
+    cv2.destroyAllWindows()
+    erode[pt1:pt2, pt3:pt4] = region
+
+erode = cv2.cvtColor(erode, cv2.COLOR_GRAY2RGB)
+# mask = erode.copy()
+# cv2.fillPoly(erode, , [255, 255, 255])
+# erode = cv2.bitwise_and(erode, mask)
 
 # For each rectangular region, calculate HOG features and predict
 # the digit using Linear SVM.
 
+# Crop off part of the image for removing noisy edge
 crop_pixel = 15
 
 for rect in rects_split:
+
     # Draw the rectangles
+    # cv2.rectangle(erode, (rect[0], rect[1]), (rect[0] + rect[2], rect[1] + rect[3]), (255, 0, 0), 3)
     (x,y,w,h) = centerCorrection(rect[0],rect[1],rect[2], rect[3])
-    # rect = []
-    # rect.append(x)
-    # rect.append(y)
-    # rect.append(w)
-    # rect.append(h)
+    rect = []
+    rect.append(x)
+    rect.append(y)
+    rect.append(w)
+    rect.append(h)
 
     cv2.rectangle(im, (rect[0], rect[1]), (rect[0] + rect[2], rect[1] + rect[3]), (0, 255, 0), 3)
+    cv2.rectangle(erode, (rect[0], rect[1]), (rect[0] + rect[2], rect[1] + rect[3]), (0, 255, 0), 3)
     # Make the rectangular region around the digit
     # leng = int(rect[3] * 1.6 - crop_pixel)
     # pt1 = int(rect[1] + rect[3] // 2 - leng // 2)
@@ -151,7 +185,7 @@ for rect in rects_split:
     pt2 = int(rect[1] + rect[3] - crop_pixel)
     pt3 = int(rect[0] + crop_pixel)
     pt4 = int(rect[0] + rect[2] - crop_pixel)
-    roi = im_th[pt1:pt2, pt3:pt4]
+    roi = erode[pt1:pt2, pt3:pt4]
     # Resize the image
     roi = cv2.resize(roi, (28, 28), interpolation=cv2.INTER_AREA)
     roi = cv2.dilate(roi, (3, 3))
@@ -161,6 +195,7 @@ for rect in rects_split:
     nbr = clf.predict(roi_hog_fd)
     cv2.putText(im, str(int(nbr[0])), (rect[0], rect[1]),cv2.FONT_HERSHEY_DUPLEX, 2, (0, 255, 255), 3)
 
+cv2.imshow("erode", erode)
 cv2.imshow("Blah", im)
 cv2.waitKey()
 
