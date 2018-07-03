7c65933ddfc730615c119ce099b72b83bf419f62
Unknown <hudongrui_01@outlook.com>
2018-07-03 11:18:04 -0400

    TestPic

--- bilateralFiltering_Canny.py
+++ bilateralFiltering_Canny.py
@@ -12,9 +12,23 @@ img = cv2.imread(args["image"])
 
 # img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-img_blurred_bilateral = cv2.bilateralFilter(img, 9, 10, 100)
+img_blurred_bilateral = cv2.bilateralFilter(img, 20, 50, 50)
 
 edges = cv2.Canny(img_blurred_bilateral, 200, 300)
+
+lines = cv2.HoughLines(edges, 1, np.pi/180, 200)
+
+for rho,theta in lines[0]:
+    a = np.cos(theta)
+    b = np.sin(theta)
+    x0 = a*rho
+    y0 = b*rho
+    x1 = int(x0 + 1000*(-b))
+    y1 = int(y0 + 1000*(a))
+    x2 = int(x0 - 1000*(-b))
+    y2 = int(y0 - 1000*(a))
+
+    cv2.line(img, (x1, y1), (x2, y2), (0, 0, 255), 2)
 
 out = np.bitwise_or(img, edges[:, :, np.newaxis])
 
